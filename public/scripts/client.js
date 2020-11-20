c92bcfa3635a599a7948ad41942a93eb5d57887b
alysoncp <alyson.c.pickett@gmail.com>
2020-11-20 08:17:13 +0000

    comments added to client side js code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,23 +1,24 @@
 /*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ * Client-side JS logic
  */
 
-
+// Wait for the document to finish loading before commencing 
 $(document).ready(function() {
 
+  // Event Listener for incorrect tweet entry error message
   $("#tweet-text").focus(() => {
     $(".error-msg").text('');
     $(".error-msg").css("visibility", "hidden");
   });
 
+  // Escape function to avoid code inject in tweet text field
   const escape =  function(str) {
     let span = document.createElement('span');
     span.appendChild(document.createTextNode(str));
     return span.innerHTML;
   };
 
+  // Function to parse the created_at data in each tweet into a friendly sec/minutes/hours/days since display.
   const dateDisplay = function(dateCreated) {
     const d = new Date();
     const n = d.getTime();
@@ -30,9 +31,12 @@ $(document).ready(function() {
       return `${Math.floor(msSince / 3600000)} hours ago`;
     } else if (msSince > 86400000) {
       return `${Math.floor(msSince / 86400000)} days ago`;
+    } else {
+      return "Unknown date"
     }
   };
 
+  // Creates a new tweet with the fetched tweet data and inserts it into an article
   const createTweetElement = function(data) {
     const tweetAge = dateDisplay(data.created_at);
     const $tweet = $(`<article class="tweet">
@@ -47,7 +51,6 @@ $(document).ready(function() {
       <span>${tweetAge}</span>
       <span> $ & @</span>
     </div>
-
     </article>`
     );
     return $tweet;
@@ -55,14 +58,14 @@ $(document).ready(function() {
 
 
   const renderTweets = function(tweetData) {
-    $('#tweets-container').empty();
+    $('#tweets-container').empty(); // Blanks the tweets container to avoid double loading
     for (elm of tweetData) {
       const newTweet = createTweetElement(elm);
-      $('#tweets-container').prepend(newTweet);
+      $('#tweets-container').prepend(newTweet); // adds tweet to top of list
     }
   };
 
- 
+  
   const loadTweets = function() {
     $.get('/tweets')
       .then(function(tweets) {
@@ -72,18 +75,23 @@ $(document).ready(function() {
 
   loadTweets();
 
+  // Declare the form path
   const $postedTweet = $('.form-inline');
+
+  // Event handler for a tweet submission
   $postedTweet.on("submit", function(event) {
-    event.preventDefault();
+    event.preventDefault(); // Don't navigate to a new page
+    // Check tweet isn't empty or too long:
     if (($("#tweet-text").val()) && ($("#tweet-text").val().length) <= 140) {
-      console.log($("#tweet-text").val().length);
       const serializedData = $(this).serialize();
       $.post("/tweets/", serializedData)
         .then(() => {
           loadTweets();
         })
+        // Empty the tweet text field
         .then(()=> $("#tweet-text").val(''))
         .then(() => $("#counter").val(140));
+    // Write error message if data entry isn't valid:    
     } else if (($("#tweet-text").val() === '')) {
       $(".error-msg").text("You have to type something...");
       $(".error-msg").css("visibility", "visible");
@@ -98,5 +106,4 @@ $(document).ready(function() {
     
 
 
-
-});
+}); // End of Document Ready
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,13 @@
+// Wait until document has loaded
 $(document).ready(function() {
-  console.log("ready");
 
+  // Update charecter anytime a change occurs in the tweet textfield
   $('#tweet-text').on("input", function() {
     const numOfChars = this.value.length;
     const counterPath = $(this).siblings("#tweet-sub").find("#counter");
     counterPath.val(140 - numOfChars);
     
-
+    // Change css styling of counter when charecters is over 140
     counterPath.css({
       color: function() {
         if (numOfChars > 140) {
@@ -17,8 +18,7 @@ $(document).ready(function() {
           
       }});
 
-
   });
 
 
-});
+}); // End of document ready
