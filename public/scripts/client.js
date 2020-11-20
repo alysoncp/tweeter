3b56af87289291a47322397d70258eef8bf6dafe
alysoncp <alyson.c.pickett@gmail.com>
2020-11-20 08:06:47 +0000

    files linted en mass

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,30 +8,30 @@
 $(document).ready(function() {
 
   $("#tweet-text").focus(() => {
-    $(".error-msg").text('')
-    $(".error-msg").css("visibility", "hidden")
-  })
+    $(".error-msg").text('');
+    $(".error-msg").css("visibility", "hidden");
+  });
 
   const escape =  function(str) {
     let span = document.createElement('span');
     span.appendChild(document.createTextNode(str));
     return span.innerHTML;
-  }
+  };
 
-  const dateDisplay = function (dateCreated) {
+  const dateDisplay = function(dateCreated) {
     const d = new Date();
     const n = d.getTime();
-    const msSince = n-dateCreated;
+    const msSince = n - dateCreated;
     if (msSince < 60000) {
-      return `${Math.floor(msSince/1000)} seconds ago`
-    } else if (msSince > 60000 && msSince < 3600000){
-      return `${Math.floor(msSince/100000)} minutes ago`
+      return `${Math.floor(msSince / 1000)} seconds ago`;
+    } else if (msSince > 60000 && msSince < 3600000) {
+      return `${Math.floor(msSince / 100000)} minutes ago`;
     } else if (msSince > 3600000 && msSince < 86400000) {
-      return `${Math.floor(msSince/3600000)} hours ago`
+      return `${Math.floor(msSince / 3600000)} hours ago`;
     } else if (msSince > 86400000) {
-      return `${Math.floor(msSince/86400000)} days ago`
+      return `${Math.floor(msSince / 86400000)} days ago`;
     }
-  }
+  };
 
   const createTweetElement = function(data) {
     const tweetAge = dateDisplay(data.created_at);
@@ -49,54 +49,54 @@ $(document).ready(function() {
     </div>
 
     </article>`
-  );
+    );
     return $tweet;
-  }
+  };
 
 
   const renderTweets = function(tweetData) {
     $('#tweets-container').empty();
-    for (elm of tweetData){
-      const newTweet = createTweetElement(elm)
+    for (elm of tweetData) {
+      const newTweet = createTweetElement(elm);
       $('#tweets-container').prepend(newTweet);
     }
-  }
+  };
 
  
   const loadTweets = function() {
     $.get('/tweets')
-     .then(function (tweets) {
-      renderTweets(tweets);
-    });
-  }
+      .then(function(tweets) {
+        renderTweets(tweets);
+      });
+  };
 
   loadTweets();
 
   const $postedTweet = $('.form-inline');
   $postedTweet.on("submit", function(event) {
     event.preventDefault();
-    if(($("#tweet-text").val()) && ($("#tweet-text").val().length) < 140) { 
-      console.log($("#tweet-text").val().length)
+    if (($("#tweet-text").val()) && ($("#tweet-text").val().length) <= 140) {
+      console.log($("#tweet-text").val().length);
       const serializedData = $(this).serialize();
       $.post("/tweets/", serializedData)
         .then(() => {
-          loadTweets()
-        }  )
+          loadTweets();
+        })
         .then(()=> $("#tweet-text").val(''))
-        .then(() => $("#counter").val(140))
-    } else if (($("#tweet-text").val() === '')){
-      $(".error-msg").text("You have to type something...")
-      $(".error-msg").css("visibility", "visible")
-    } else if (($("#tweet-text").val().length > 140)){
-      $(".error-msg").text("Too many charecters!!")
-      $(".error-msg").css("visibility", "visible")
+        .then(() => $("#counter").val(140));
+    } else if (($("#tweet-text").val() === '')) {
+      $(".error-msg").text("You have to type something...");
+      $(".error-msg").css("visibility", "visible");
+    } else if (($("#tweet-text").val().length > 140)) {
+      $(".error-msg").text("Too many charecters!!");
+      $(".error-msg").css("visibility", "visible");
     } else {
-      $(".error-msg").text("Something weird happened... sorry")
-      $(".error-msg").css("visibility", "visible")
+      $(".error-msg").text("Something weird happened... sorry");
+      $(".error-msg").css("visibility", "visible");
     }
   });
     
 
 
 
-});  
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
   console.log("ready");
 
-  $( '#tweet-text' ).on("input", function() {
+  $('#tweet-text').on("input", function() {
     const numOfChars = this.value.length;
-    const counterPath = $(this).siblings("#tweet-sub").find("#counter")
-    counterPath.val(140 - numOfChars)
+    const counterPath = $(this).siblings("#tweet-sub").find("#counter");
+    counterPath.val(140 - numOfChars);
     
 
     counterPath.css({
-      color: function(){
-          if (numOfChars > 140){
-              return 'red';
-          } else {
-              return "#545149";
-          }
+      color: function() {
+        if (numOfChars > 140) {
+          return 'red';
+        } else {
+          return "#545149";
+        }
           
-    }});
+      }});
 
 
   });
